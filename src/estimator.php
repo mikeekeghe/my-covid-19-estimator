3451e83b6bd1bb882f6f33e37ac95c0a358c2ee7
Michael Ekeghe <36678934+techlinesolutions@users.noreply.github.com>
2020-04-07 19:23:00 +0100

    Update estimator.php

--- src/estimator.php
+++ src/estimator.php
@@ -1,6 +1,22 @@
 <?php
+//get data from input
+$json = $_GET['json'];
+
+$data = json_decode($json);
 
 function covid19ImpactEstimator($data)
 {
-  return $data;
+  $reportedCases = $data->reportedCases;
+
+  // 1A. currentlyInfected =  reportedCases * 10).
+  $impact->currentlyInfected =  $reportedCases * 10;
+
+  //1B
+  $severeImpact->currentlyInfected =$reportedCases * 50;
+
+  //1C
+  $impact->infectionsByRequestedTime = $impact->currentlyInfected * 512;
+  $severeImpact->infectionsByRequestedTime = $severeImpact->currentlyInfected * 512;
+
+    return $data;
 }
